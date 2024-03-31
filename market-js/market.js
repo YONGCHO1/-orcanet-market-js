dadcba7438eab0fd31c310d46ffdb628e439a2cb
Yongjun Cho <guncho0918@gmail.com>
2024-03-31 17:53:22 -0400

    working on grpc

--- market-js/market.js
+++ market-js/market.js
@@ -27,11 +27,12 @@ var packageDefinition = protoLoader.loadSync(
         defaults: true,
         oneofs: true
     });
+
 var market_proto = grpc.loadPackageDefinition(packageDefinition).market;
 
 const bootstrapPeers = [];
 
-const makeNode = async () => {
+const makeNode = async (mode) => {
     const nodes = await createLibp2p({
         addresses: {
             listen: ['/ip4/0.0.0.0/tcp/0']
@@ -43,7 +44,8 @@ const makeNode = async () => {
         // peerDiscovery: [bootstrap()],
         services: {
             kadDHT: kadDHT({
-                kBucketSize: 20
+                kBucketSize: 20,
+                clientMode: mode,
             }),
         }
     });
@@ -54,7 +56,7 @@ const makeNode = async () => {
 
 
 // Create new node and start it
-const node = await makeNode();
+var node = await makeNode(false);
 
 // Put or update values to corresponding key
 function putOrUpdateKeyValue(node, cid, value) {
@@ -106,10 +108,10 @@ function checkProvider(node, cid) {
 
 // This function registers a file and user into the servers HashMap 
 function registerFile(call, callback) {
-
+    console.log("------------------register file---------------------");
     let newUser = call.request.user;
     let cid = call.request.fileHash;
-    console.log("------------------register file---------------------");
+    // console.log("------------------register file---------------------");
   
   
   
@@ -159,6 +161,7 @@ async function main() {
     var argv = process.argv.slice(2);
 
     console.log(argv[1]);
+    var isCLient = false;
 
     switch (argv[0]) {
         case '-bootstrap':
@@ -168,35 +171,136 @@ async function main() {
                 // add bootstrap node through process
                 bootstrapPeers.push(argv[1]);
             break;
-        case '-clientMode':
-
+        case '-clientmode':
+            isCLient = true;
+            node = await makeNode(true);
     }
+
 
     // Create new node and start it
     // const node = await makeNode();
+    if (isCLient){
+        console.log("say hi in client mode");
+ 
+        var target;
+        // if (argv.target) {
+        // target = argv.target;
+        // } else {
+        // target = '10.1.185.0';
+        // }
 
-    console.log('Peer ID:', node.peerId.toString());
-    console.log('Connect to me on:');
-    const multiaddresses = node.getMultiaddrs();
-    multiaddresses.forEach(addr => console.log(addr.toString()));
+        // var target1 = 
+        var my_ip
+        var my_port
+        const multiaddresses = node.getMultiaddrs();
+        multiaddresses.forEach(addr => {
+            console.log(addr.toString())
+            let addrs = addr.toString();
+            let addr_info = addrs.split('/');
+            my_ip = addr_info[2];
+            my_port = addr_info[4];
+        });
 
-    const bootstrapAddresses = await Promise.all(bootstrapPeers.map(async (addr) => {
-        console.log("get into bootstrap function");
-        try {
-            console.log("get into try");
-            console.log(addr);
-            const peerAddr = multiaddr(addr);
-            const peerInfo = await node.dial(peerAddr, {
-                signal: AbortSignal.timeout(10_000)
-            });
+        // TO DO:
+        // I think we need to work on decide target and figure out what does target mean and work for.
+        target = my_ip+ ':'+my_port;
+        console.log("Before Passing to connect proto file");
+        var client = new market_proto.Market(target, grpc.credentials.createInsecure());
 
-            console.log('Connected to bootstrap peer:', peerInfo.id.toString());
-            return peerInfo;
-        } catch (error) {
-            console.error('Failed to connect to bootstrap peer:', error);
-            return null;
+        console.log("Passed to connect proto file");
+
+        var newUser = {
+        id: 1, // will be replaced by id given from Peer Node team
+        name: argv[1],
+        ip: my_ip,
+        port: my_port,
+        price: argv[2],
         }
-    }));
+
+        console.log(newUser);
+
+        client.registerFile({ user: newUser, fileHash: "230942459824" }, function (err, response) {
+            console.log("error: "+err);
+            console.log("RegisterFile Response");
+        });
+
+    }
+    else {
+        console.log('Peer ID:', node.peerId.toString());
+        console.log('Connect to me on:');
+
+        let my_ip
+        let my_port
+
+        const multiaddresses = node.getMultiaddrs();
+        multiaddresses.forEach(addr => {
+            console.log(addr.toString());
+            let addrs = addr.toString();
+            let addr_info = addrs.split('/');
+            my_ip = addr_info[2];
+            my_port = addr_info[4];
+        });
+
+        // TO DO:
+        // Like deciding target variable, we need to figure out bind address too.
+        // I(Jun) think there is a relationship between target and bind address
+        let bindAddress = '0.0.0.0:3000';
+
+        console.log(bindAddress);
+
+        const server = new grpc.Server();
+        server.addService(market_proto.Market.service, { RegisterFile: registerFile, CheckHolders: checkHolders });
+        server.bindAsync(bindAddress, grpc.ServerCredentials.createInsecure(), 
+            (err) => !err ? server.start() : console.log(err));
+
+        const bootstrapAddresses = await Promise.all(bootstrapPeers.map(async (addr) => {
+            console.log("get into bootstrap function");
+            try {
+                console.log("get into try");
+                console.log(addr);
+                const peerAddr = multiaddr(addr);
+                const peerInfo = await node.dial(peerAddr, {
+                    signal: AbortSignal.timeout(10_000)
+                });
+
+                console.log('Connected to bootstrap peer:', peerAddr.getPeerId()); //peerInfo.id.toString());
+                return peerInfo;
+            } catch (error) {
+                console.error('Failed to connect to bootstrap peer:', error);
+                return null;
+            }
+        }));
+
+        if (bootstrapAddresses.length === 0) {
+            await new Promise(() => { }); // Keep the program running
+        }
+        else {
+            const dht = await createLibp2p({
+                addresses: {
+                    listen: ['/ip4/0.0.0.0/tcp/0']
+                },
+                transports: [tcp()],
+                streamMuxers: [mplex()],
+                connectionEncryption: [noise()],
+                peerDiscovery: [
+                    bootstrap({
+                        list: bootstrapAddresses.filter(addr => addr !== null)
+                      })
+                ],
+                services: {
+                    kadDHT: kadDHT({
+                      kBucketSize: 20
+                    }),
+                }
+            });
+    
+            await dht.start();
+        }
+
+        await new Promise(() => { }); // Keep the program running
+    }
+    
+   
 
     // const dht1 = new kadDHT({
     //     libp2p: node,
@@ -209,51 +313,26 @@ async function main() {
 
     // const dht2 = await makeNode();
 
-    const dht = await createLibp2p({
-        addresses: {
-            listen: ['/ip4/0.0.0.0/tcp/0']
-        },
-        transports: [tcp()],
-        streamMuxers: [mplex()],
-        connectionEncryption: [noise()],
-        peerDiscovery: [
-            bootstrap({
-                list: bootstrapAddresses.filter(addr => addr !== null)
-              })
-        ],
-        services: {
-            kadDHT: kadDHT({
-              kBucketSize: 20
-            }),
-        }
-    });
+    // const dht = await createLibp2p({
+    //     addresses: {
+    //         listen: ['/ip4/0.0.0.0/tcp/0']
+    //     },
+    //     transports: [tcp()],
+    //     streamMuxers: [mplex()],
+    //     connectionEncryption: [noise()],
+    //     peerDiscovery: [
+    //         bootstrap({
+    //             list: bootstrapAddresses.filter(addr => addr !== null)
+    //           })
+    //     ],
+    //     services: {
+    //         kadDHT: kadDHT({
+    //           kBucketSize: 20
+    //         }),
+    //     }
+    // });
 
-    if (bootstrapAddresses.length === 0) {
-        await new Promise(() => { }); // Keep the program running
-    }
-    else {
-        const dht = await createLibp2p({
-            addresses: {
-                listen: ['/ip4/0.0.0.0/tcp/0']
-            },
-            transports: [tcp()],
-            streamMuxers: [mplex()],
-            connectionEncryption: [noise()],
-            peerDiscovery: [
-                bootstrap({
-                    list: bootstrapAddresses.filter(addr => addr !== null)
-                  })
-            ],
-            services: {
-                kadDHT: kadDHT({
-                  kBucketSize: 20
-                }),
-            }
-        });
-
-        await dht.start();
-    }
-
+   
 
     // await dht.start();
     // await dht.bootstrap(bootstrapAddresses.filter(addr => addr !== null));
@@ -268,13 +347,7 @@ async function main() {
     //         .catch((error) => console.error('Failed to connect to peer:', peerInfo.id.toB58String(), error));
     // });
 
-    const server = new grpc.Server();
-    server.addService(market_proto.Market.service, { RegisterFile: registerFile, CheckHolders: checkHolders });
-    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-        server.start();
-    });  
-
-    await new Promise(() => { }); // Keep the program running
+    
 }
 
 main().catch((error) => {
